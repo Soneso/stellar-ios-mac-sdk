ecb03010f64ad74845b271e2715ba1e601cc1bce
Razvan Chelemen <chelemen.razvan@gmail.com>
2018-01-26 09:53:29 +0200

    Added basic class.

---
+++ stellarsdk/stellarsdk/StellarSDK.swift
@@ -0,0 +1,13 @@
+//
+//  StellarSDK.swift
+//  stellarsdk
+//
+//  Created by Razvan Chelemen on 26/01/2018.
+//  Copyright © 2018 Soneso. All rights reserved.
+//
+
+import Foundation
+
+public class StellarSDK: NSObject {
+
+}
--- stellarsdk/stellarsdk/stellarsdk.h
+++ stellarsdk/stellarsdk/stellarsdk.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double stellarsdkVersionNumber;
 FOUNDATION_EXPORT const unsigned char stellarsdkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <stellarsdk/PublicHeader.h>
-
-
