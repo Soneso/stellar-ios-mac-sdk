cb27bd5f1568e224432268c7ba83412aa2dc472e
Razvan Chelemen <chelemen.razvan@gmail.com>
2018-01-26 09:30:09 +0200

    Added xcode project.

---
+++ stellarsdk/stellarsdk/stellarsdk.h
@@ -0,0 +1,19 @@
+//
+//  stellarsdk.h
+//  stellarsdk
+//
+//  Created by Razvan Chelemen on 26/01/2018.
+//  Copyright © 2018 Soneso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for stellarsdk.
+FOUNDATION_EXPORT double stellarsdkVersionNumber;
+
+//! Project version string for stellarsdk.
+FOUNDATION_EXPORT const unsigned char stellarsdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <stellarsdk/PublicHeader.h>
+
+
