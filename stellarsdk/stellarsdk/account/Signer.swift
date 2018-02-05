87406484b9b68580de84e9613f175aba67df0503
Christian Rogobete <c.rogobete@soneso.com>
2018-02-05 19:13:33 +0100

    Improve accounts

--- stellarsdk/stellarsdk/account/Signer.swift
+++ stellarsdk/stellarsdk/account/Signer.swift
@@ -12,15 +12,31 @@ public class Signer: NSObject, Codable {
     
     public var publicKey:String
     public var weight:Int
-    
-    override init() {
-        publicKey = ""
-        weight = 0
-    }
+    public var key:String!
+    public var type:String!
     
     enum CodingKeys: String, CodingKey {
         case publicKey = "public_key"
         case weight
+        case key
+        case type
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        publicKey = try values.decode(String.self, forKey: .publicKey)
+        weight = try values.decode(Int.self, forKey: .weight)
+        key = try values.decodeIfPresent(String.self, forKey: .key)
+        type = try values.decodeIfPresent(String.self, forKey: .type)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(publicKey, forKey: .publicKey)
+        try container.encode(weight, forKey: .weight)
+        try container.encode(key, forKey: .key)
+        try container.encode(type, forKey: .type)
     }
     
 }
