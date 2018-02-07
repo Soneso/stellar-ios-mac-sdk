b26e0a5ee016c178d31b628992f5ee6b5168df9d
Christian Rogobete <c.rogobete@soneso.com>
2018-02-07 23:50:23 +0100

    Add documentation to Balance

--- stellarsdk/stellarsdk/account/Balance.swift
+++ stellarsdk/stellarsdk/account/Balance.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
+///  Represents the account balance.
+///  See [Horizon API](https://www.stellar.org/developers/horizon/reference/resources/account.html "Account Balance")
 public class Balance: NSObject, Codable {
 
+    /// Balance for the specified asset.
     public var balance:String
+    
+    /// Maximum number of asset amount this account can hold.
     public var limit:String!
+    
+    /// The asset type. Possible values: native, credit_alphanum4, credit_alphanum12
+    /// See also Constants.AssetType
     public var assetType:String
+    
+    /// The asset code e.g., USD or BTC.
     public var assetCode:String?
+    
+    /// The account id of the account that created the asset.
     public var assetIssuer:String?
     
+    // Properties to encode and decode.
     enum CodingKeys: String, CodingKey {
         case balance
         case limit
@@ -24,8 +37,12 @@ public class Balance: NSObject, Codable {
         case assetIssuer = "asset_issuer"
     }
     
+    /**
+        Initializer - creates a new instance by decoding from the given decoder.
+     
+        - Parameter decoder: The decoder containing the data
+     */
     public required init(from decoder: Decoder) throws {
-        
         let values = try decoder.container(keyedBy: CodingKeys.self)
         balance = try values.decode(String.self, forKey: .balance) as String
         limit = try values.decodeIfPresent(String.self, forKey: .limit)
@@ -34,6 +51,11 @@ public class Balance: NSObject, Codable {
         assetIssuer = try values.decodeIfPresent(String.self, forKey: .assetIssuer)
     }
     
+    /**
+        Initializer - creates a new instance by decoding from the given decoder.
+     
+        - Parameter decoder: The decoder containing the data
+     */
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(balance, forKey: .balance)
@@ -42,5 +64,4 @@ public class Balance: NSObject, Codable {
         try container.encode(assetCode, forKey: .assetCode)
         try container.encode(assetIssuer, forKey: .assetIssuer)
     }
-    
 }
