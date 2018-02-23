2b6d2cc625e1ca666144290dee104f8763035dac
Razvan Chelemen <chelemen.razvan@gmail.com>
2018-02-23 16:38:03 +0200

    Fix merge issue.

--- stellarsdk/stellarsdkTests/transactions/TransactionsRemoteTestCase.swift
+++ stellarsdk/stellarsdkTests/transactions/TransactionsRemoteTestCase.swift
@@ -116,18 +116,109 @@ class TransactionsRemoteTestCase: XCTestCase {
         wait(for: [expectation], timeout: 15.0)
     }
     
+    func testSendAndReceivePayment() {
+        
+        let expectation = XCTestExpectation(description: "Payment successfully sent and received")
+        
+        do {
+            let sourceAccountKeyPair = try KeyPair(secretSeed:"SDXEJKRXYLTV344KWCRJ4PAGAJVXKGK3UGESRWBWLDEWYO4S5OQ6VQ6I")
+            let destinationAccountKeyPair = try KeyPair(accountId: "GCKECJ5DYFZUX6DMTNJFHO2M4QKTUO5OS5JZ4EIIS7C3VTLIGXNGRTRC")
+            
+            func send() {
+                sdk.accounts.getAccountDetails(accountId: sourceAccountKeyPair.accountId) { (response) -> (Void) in
+                    switch response {
+                    case .success(let accountResponse):
+                        do {
+                            let paymentOperation = PaymentOperation(sourceAccount: sourceAccountKeyPair,
+                                                                    destination: destinationAccountKeyPair,
+                                                                    asset: Asset(type: AssetType.ASSET_TYPE_NATIVE)!,
+                                                                    amount: 1.5)
+                            let transaction = try Transaction(sourceAccount: accountResponse,
+                                                              operations: [paymentOperation],
+                                                              memo: Memo.none,
+                                                              timeBounds:nil)
+                            try transaction.sign(keyPair: sourceAccountKeyPair, network: Network.testnet)
+                            
+                            try self.sdk.transactions.submitTransaction(transaction: transaction) { (response) -> (Void) in
+                                switch response {
+                                case .success(_):
+                                    receive()
+                                case .failure(_):
+                                    XCTAssert(false)
+                                    expectation.fulfill()
+                                }
+                            }
+                        } catch {
+                            XCTAssert(false)
+                            expectation.fulfill()
+                        }
+                    case .failure(_):
+                        XCTAssert(false)
+                        expectation.fulfill()
+                    }
+                }
+            }
+            
+            func receive() {
+                sdk.payments.stream(for: .paymentsForAccount(account: destinationAccountKeyPair.accountId, cursor: nil)).onReceive { (response) -> (Void) in
+                    switch response {
+                        case .open:
+                            break
+                        case .response(let id, let operationResponse):
+                            if let paymentResponse = operationResponse as? PaymentOperationResponse {
+                                print("Payment of \(paymentResponse.amount) XLM from \(paymentResponse.sourceAccount) received -  id \(id)" )
+                                showDestinationAccountBalance(finish: true)
+                            }
+                        case .error( _):
+                            XCTAssert(false)
+                            expectation.fulfill()
+                    }
+                }
+            }
+            
+            func showDestinationAccountBalance(finish:Bool) {
+                sdk.accounts.getAccountDetails(accountId: destinationAccountKeyPair.accountId) { (response) -> (Void) in
+                    switch response {
+                    case .success(let accountResponse):
+                        for balance in accountResponse.balances {
+                            if balance.assetType == AssetTypeAsString.NATIVE {
+                                print("Destination account balance: \(balance.balance) XLM")
+                                if (finish) {
+                                    XCTAssert(true)
+                                    expectation.fulfill()
+                                }
+                            }
+                        }
+                    case .failure(_):
+                        XCTAssert(false)
+                        expectation.fulfill()
+                    }
+                }
+            }
+            
+            showDestinationAccountBalance(finish: false)
+            send()
+            
+        } catch {
+            XCTAssert(false)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 15.0)
+    }
+    
     func testTransactionSigning() {
         let publicKey = Data(base64Encoded:"uHFsF4DaBlIsPUzFlMuBFkgEROGR9DlEBYCg3x+V72A=")!
         let privateKey = Data(base64Encoded: "KJJ6vrrDOe9XIDAj6iSftUzux0qWwSwf3er27YKUOU2ZbT/G/wqFm/tDeez3REW5YlD5mrf3iidmGjREBzOEjQ==")!
         let keyPair = try! KeyPair(publicKey: PublicKey([UInt8](publicKey)), privateKey: PrivateKey([UInt8](privateKey)))
         
-        let expectation = XCTestExpectation(description: "Get transaction details")
+        let expectation = XCTestExpectation(description: "Transaction successfully signed.")
         sdk.accounts.getAccountDetails(accountId: keyPair.accountId) { (response) -> (Void) in
             switch response {
             case .success(let data):
                 let operationBody = OperationBodyXDR.inflation
                 let operation = OperationXDR(sourceAccount: keyPair.publicKey, body: operationBody)
-                var transaction = TransactionXDR(sourceAccount: keyPair.publicKey, seqNum: UInt64(data.sequenceNumber)! + 1, timeBounds: nil, memo: .none, operations: [operation])
+                var transaction = TransactionXDR(sourceAccount: keyPair.publicKey, seqNum: data.sequenceNumber + 1, timeBounds: nil, memo: .none, operations: [operation])
                 
                 try! transaction.sign(keyPair: keyPair, network: .testnet)
                 let xdrEnvelope = try! transaction.encodedEnvelope()
@@ -141,7 +232,7 @@ class TransactionsRemoteTestCase: XCTestCase {
         wait(for: [expectation], timeout: 15.0)
     }
     
-    func testTransactionPost() {
+    func testTransactionEnvelopePost() {
         let expectation = XCTestExpectation(description: "Get transaction details")
         let xdrEnvelope = "AAAAALhxbBeA2gZSLD1MxZTLgRZIBEThkfQ5RAWAoN8fle9gAAAAZAByE3sAAAAIAAAAAAAAAAAAAAABAAAAAQAAAAC4cWwXgNoGUiw9TMWUy4EWSARE4ZH0OUQFgKDfH5XvYAAAAAkAAAAAAAAAAR+V72AAAABAAuiJ2+1FGpG7D+sS9qqZlk2/dsu8mdECuR1jiX9PaawJaJMETUP6u06cZgzrqopzmypJMOS/ob7BRvCQ3JkwDg=="
         
@@ -157,14 +248,14 @@ class TransactionsRemoteTestCase: XCTestCase {
         wait(for: [expectation], timeout: 25.0)
     }
     
-  func testTransactionsStream() {
+    func testTransactionsStream() {
         let expectation = XCTestExpectation(description: "Get response from stream")
         
         sdk.transactions.stream(for: .allTransactions(cursor: nil)).onReceive { (response) -> (Void) in
             switch response {
             case .open:
                 break
-            case .response(_,_):
+            case .response( _, _):
                 expectation.fulfill()
             case .error( _):
                 break
@@ -173,7 +264,7 @@ class TransactionsRemoteTestCase: XCTestCase {
         
         wait(for: [expectation], timeout: 15.0)
     }
- /*
+/*
     func testTransactionsForAccountStream() {
         let expectation = XCTestExpectation(description: "Get response from stream")
         
