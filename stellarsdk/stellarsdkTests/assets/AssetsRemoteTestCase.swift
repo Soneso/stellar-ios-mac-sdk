3300aa7f6d65bfa5d47161f58ed8b0a9054c6152
Christian Rogobete <c.rogobete@soneso.com>
2022-05-30 20:11:19 +0200

    improve assets and effects tests

--- stellarsdk/stellarsdkTests/assets/AssetsRemoteTestCase.swift
+++ stellarsdk/stellarsdkTests/assets/AssetsRemoteTestCase.swift
@@ -12,87 +12,145 @@ import stellarsdk
 class AssetsRemoteTestCase: XCTestCase {
     let sdk = StellarSDK()
     
+    let testKeyPair = try! KeyPair.generateRandomKeyPair()
+    let IOMIssuingAccountKeyPair = try! KeyPair.generateRandomKeyPair()
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let expectation = XCTestExpectation(description: "accounts prepared for tests")
+
+        let testAccountId = testKeyPair.accountId
+        let issuingAccountId = IOMIssuingAccountKeyPair.accountId
+        
+        let IOMAsset = ChangeTrustAsset(canonicalForm: "IOM:" + issuingAccountId)!
+        let changeTrustOp = ChangeTrustOperation(sourceAccountId:testAccountId, asset:IOMAsset, limit: 100000000)
+        let paymentOp = try! PaymentOperation(sourceAccountId: issuingAccountId, destinationAccountId: testAccountId, asset: IOMAsset, amount: 20000.0)
+
+        sdk.accounts.createTestAccount(accountId: testAccountId) { (response) -> (Void) in
+            switch response {
+            case .success(_):
+                self.sdk.accounts.createTestAccount(accountId: issuingAccountId) { (response) -> (Void) in
+                    switch response {
+                    case .success(_):
+                        self.sdk.accounts.getAccountDetails(accountId: testAccountId) { (response) -> (Void) in
+                        switch response {
+                        case .success(let accountResponse):
+                            let transaction = try! Transaction(sourceAccount: accountResponse,
+                                                              operations: [changeTrustOp, paymentOp],
+                                                              memo: Memo.none)
+                            try! transaction.sign(keyPair: self.testKeyPair, network: Network.testnet)
+                            try! transaction.sign(keyPair: self.IOMIssuingAccountKeyPair, network: Network.testnet)
+                            
+                            try! self.sdk.transactions.submitTransaction(transaction: transaction) { (response) -> (Void) in
+                                switch response {
+                                case .success(let response):
+                                    print("setUp: Transaction successfully sent. Hash:\(response.transactionHash)")
+                                    expectation.fulfill()
+                                default:
+                                    XCTFail()
+                                }
+                            }
+                        case .failure(_):
+                            XCTFail()
+                        }
+                    }
+                    case .failure(_):
+                        XCTFail()
+                    }
+                }
+            case .failure(_):
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 25.0)
     }
+    
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testGetAssets() {
-        let expectation = XCTestExpectation(description: "Get assets and parse their details successfully")
+    func testAll() {
+        getAssets()
+        getAssetAccountsAndBalances()
+    }
         
-        sdk.assets.getAssets(order:Order.descending, limit:2) { (response) -> (Void) in
-            switch response {
-            case .success(let assetsResponse):
-                // load next page
-                assetsResponse.getNextPage(){ (response) -> (Void) in
-                    switch response {
-                    case .success(let nextAssetsResponse):
-                        // load previous page, should contain the same assets as the first page
-                        nextAssetsResponse.getPreviousPage(){ (response) -> (Void) in
-                            switch response {
-                            case .success(let prevAssetsResponse):
-                                let asset1 = assetsResponse.records.first
-                                let asset2 = prevAssetsResponse.records.last // because ordering is asc now.
-                                XCTAssertTrue(asset1?.amount == asset2?.amount)
-                                XCTAssertTrue(asset1?.assetType == asset2?.assetType)
-                                if (asset1?.assetType != AssetTypeAsString.NATIVE) {
-                                    XCTAssertTrue(asset1?.assetCode == asset2?.assetCode)
-                                    XCTAssertTrue(asset1?.assetIssuer == asset2?.assetIssuer)
+    func getAssets() {
+        XCTContext.runActivity(named: "getAssets") { activity in
+            let expectation = XCTestExpectation(description: "Get assets and parse their details successfully")
+            
+            sdk.assets.getAssets(order:Order.descending, limit:2) { (response) -> (Void) in
+                switch response {
+                case .success(let assetsResponse):
+                    // load next page
+                    assetsResponse.getNextPage(){ (response) -> (Void) in
+                        switch response {
+                        case .success(let nextAssetsResponse):
+                            // load previous page, should contain the same assets as the first page
+                            nextAssetsResponse.getPreviousPage(){ (response) -> (Void) in
+                                switch response {
+                                case .success(let prevAssetsResponse):
+                                    let asset1 = assetsResponse.records.first
+                                    let asset2 = prevAssetsResponse.records.last // because ordering is asc now.
+                                    XCTAssertTrue(asset1?.amount == asset2?.amount)
+                                    XCTAssertTrue(asset1?.assetType == asset2?.assetType)
+                                    if (asset1?.assetType != AssetTypeAsString.NATIVE) {
+                                        XCTAssertTrue(asset1?.assetCode == asset2?.assetCode)
+                                        XCTAssertTrue(asset1?.assetIssuer == asset2?.assetIssuer)
+                                    }
+                                    XCTAssert(true)
+                                    expectation.fulfill()
+                                case .failure(let error):
+                                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
+                                    XCTFail()
                                 }
-                                XCTAssert(true)
-                                expectation.fulfill()
-                            case .failure(let error):
-                                StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
-                                XCTAssert(false)
                             }
+                        case .failure(let error):
+                            StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
+                            XCTFail()
                         }
-                    case .failure(let error):
-                        StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
-                        XCTAssert(false)
                     }
+                case .failure(let error):
+                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
+                    XCTFail()
+                    expectation.fulfill()
                 }
-            case .failure(let error):
-                StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
-                XCTAssert(false)
-                expectation.fulfill()
             }
+            wait(for: [expectation], timeout: 15.0)
         }
-        wait(for: [expectation], timeout: 15.0)
     }
     
-    func testGetAssetAccountsAndBalances() {
-        let expectation = XCTestExpectation(description: "Get asset details successfully")
-        
-        sdk.assets.getAssets(for: "SONESO") { (response) -> (Void) in
-            switch response {
-            case .success(let assetsResponse):
-                if let asset = assetsResponse.records.first {
-                    let accounts = asset.accounts
-                    XCTAssert(accounts.authorized == 1)
-                    XCTAssert(accounts.authorizedToMaintainLiabilities == 0)
-                    XCTAssert(accounts.unauthorized == 0)
-                    XCTAssert(asset.numberOfAccounts == 1)
-                    XCTAssert(asset.numClaimableBalances == 0)
-                    XCTAssert(asset.claimableBalancesAmount == 0.0)
-                    XCTAssert(asset.amount == 20000.0)
-                    let balances = asset.balances
-                    XCTAssert(balances.authorized == 20000.0)
-                    
-                } else {
-                    XCTAssert(false)
+    func getAssetAccountsAndBalances() {
+        XCTContext.runActivity(named: "getAssetAccountsAndBalances") { activity in
+            let expectation = XCTestExpectation(description: "Get asset details successfully")
+            
+            sdk.assets.getAssets(for: "IOM", assetIssuer: IOMIssuingAccountKeyPair.accountId) { (response) -> (Void) in
+                switch response {
+                case .success(let assetsResponse):
+                    if let asset = assetsResponse.records.first {
+                        let accounts = asset.accounts
+                        XCTAssert(accounts.authorized == 1)
+                        XCTAssert(accounts.authorizedToMaintainLiabilities == 0)
+                        XCTAssert(accounts.unauthorized == 0)
+                        XCTAssert(asset.numberOfAccounts == 1)
+                        XCTAssert(asset.numClaimableBalances == 0)
+                        XCTAssert(asset.claimableBalancesAmount == 0.0)
+                        XCTAssert(asset.amount == 20000.0)
+                        let balances = asset.balances
+                        XCTAssert(balances.authorized == 20000.0)
+                        
+                    } else {
+                        XCTFail()
+                    }
+                    expectation.fulfill()
+                case .failure(let error):
+                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
+                    XCTFail()
+                    expectation.fulfill()
                 }
-                expectation.fulfill()
-            case .failure(let error):
-                StellarSDKLog.printHorizonRequestErrorMessage(tag:"Load assets testcase", horizonRequestError: error)
-                XCTAssert(false)
-                expectation.fulfill()
             }
+            wait(for: [expectation], timeout: 15.0)
         }
-        wait(for: [expectation], timeout: 15.0)
     }
 }
--- stellarsdk/stellarsdkTests/effects/EffectsRemoteTestCase.swift
+++ stellarsdk/stellarsdkTests/effects/EffectsRemoteTestCase.swift
@@ -12,9 +12,48 @@ import stellarsdk
 class EffectsRemoteTestCase: XCTestCase {
     let sdk = StellarSDK()
     
+    let testKeyPair = try! KeyPair.generateRandomKeyPair()
+    var transactionHash:String? = nil
+    var ledger:Int? = nil
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let expectation = XCTestExpectation(description: "accounts prepared for tests")
+
+        let testAccountId = testKeyPair.accountId
+        let manageDataOp = ManageDataOperation(sourceAccountId: testAccountId, name: "soneso", data: "is super".data(using: .utf8))
+
+        self.sdk.accounts.createTestAccount(accountId: testAccountId) { (response) -> (Void) in
+            switch response {
+            case .success(_):
+                self.sdk.accounts.getAccountDetails(accountId: testAccountId) { (response) -> (Void) in
+                switch response {
+                case .success(let accountResponse):
+                    let transaction = try! Transaction(sourceAccount: accountResponse,
+                                                      operations: [manageDataOp],
+                                                      memo: Memo.none)
+                    try! transaction.sign(keyPair: self.testKeyPair, network: Network.testnet)
+                    
+                    try! self.sdk.transactions.submitTransaction(transaction: transaction) { (response) -> (Void) in
+                        switch response {
+                        case .success(let response):
+                            print("setUp: Transaction successfully sent. Hash:\(response.transactionHash)")
+                            self.transactionHash = response.transactionHash
+                            self.ledger = response.ledger
+                            expectation.fulfill()
+                        default:
+                            XCTFail()
+                        }
+                    }
+                case .failure(_):
+                    XCTFail()
+                }
+            }
+            case .failure(_):
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 25.0)
     }
     
     override func tearDown() {
@@ -22,203 +61,143 @@ class EffectsRemoteTestCase: XCTestCase {
         super.tearDown()
     }
     
-    func testGetEffects() {
-        let expectation = XCTestExpectation(description: "Get effects and parse their details successfully")
+    func testAll() {
+        getEffects()
+        getEffectsForAccount()
+        getEffectsForOperation()
+        getEffectsForLedger()
+        getEffectsForTransaction()
+    }
         
-        sdk.effects.getEffects { (response) -> (Void) in
-            switch response {
-            case .success(let effectsResponse):
-                // load next page
-                effectsResponse.getNextPage(){ (response) -> (Void) in
-                    switch response {
-                    case .success(let nextEffectsResponse):
-                        // load previous page, should contain the same effects as the first page
-                        nextEffectsResponse.getPreviousPage(){ (response) -> (Void) in
+    
+    func getEffects() {
+        XCTContext.runActivity(named: "getEffects") { activity in
+            let expectation = XCTestExpectation(description: "Get effects and parse their details successfully")
+            
+            sdk.effects.getEffects { (response) -> (Void) in
+                switch response {
+                case .success(let effectsResponse):
+                    // load next page
+                    effectsResponse.getNextPage(){ (response) -> (Void) in
+                        switch response {
+                        case .success(let nextEffectsResponse):
+                            // load previous page, should contain the same effects as the first page
+                            nextEffectsResponse.getPreviousPage(){ (response) -> (Void) in
+                                switch response {
+                                case .success(let prevEffectsResponse):
+                                    let effect1 = effectsResponse.records.first
+                                    let effect2 = prevEffectsResponse.records.last // because ordering is asc now.
+                                    XCTAssertTrue(effect1?.id == effect2?.id)
+                                    XCTAssertTrue(effect1?.account == effect2?.account)
+                                    XCTAssertTrue(effect1?.effectType == effect2?.effectType)
+                                    XCTAssertTrue(effect1?.effectTypeString == effect2?.effectTypeString)
+                                    XCTAssert(true)
+                                    expectation.fulfill()
+                                case .failure(let error):
+                                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"GE Test", horizonRequestError: error)
+                                    XCTAssert(false)
+                                }
+                            }
+                        case .failure(let error):
+                            StellarSDKLog.printHorizonRequestErrorMessage(tag:"GE Test", horizonRequestError: error)
+                            XCTAssert(false)
+                        }
+                    }
+                case .failure(let error):
+                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"GE Test", horizonRequestError: error)
+                    XCTAssert(false)
+                    expectation.fulfill()
+                }
+            }
+            wait(for: [expectation], timeout: 15.0)
+        }
+    }
+    
+    func getEffectsForAccount() {
+        XCTContext.runActivity(named: "getEffectsForAccount") { activity in
+            let expectation = XCTestExpectation(description: "Get effects for account and parse their details successfuly")
+            
+            sdk.effects.getEffects(forAccount: testKeyPair.accountId, order:Order.descending) { (response) -> (Void) in
+                switch response {
+                case .success(_):
+                    XCTAssert(true)
+                case .failure(let error):
+                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"GEFA Test", horizonRequestError: error)
+                    XCTAssert(false)
+                }
+                
+                expectation.fulfill()
+            }
+            wait(for: [expectation], timeout: 15.0)
+        }
+    }
+    
+    func getEffectsForOperation() {
+        XCTContext.runActivity(named: "getEffectsForOperation") { activity in
+            let expectation = XCTestExpectation(description: "Get effects for operation")
+            sdk.operations.getOperations(forAccount: testKeyPair.accountId, from: nil, order: Order.descending, includeFailed: true, join: "transactions") { (response) -> (Void) in
+                switch response {
+                case .success(let operations):
+                    XCTAssertNotNil(operations.records.first)
+                    if let operation = operations.records.first {
+                        self.sdk.effects.getEffects(forOperation: operation.id) { (response) -> (Void) in
                             switch response {
-                            case .success(let prevEffectsResponse):
-                                let effect1 = effectsResponse.records.first
-                                let effect2 = prevEffectsResponse.records.last // because ordering is asc now.
-                                XCTAssertTrue(effect1?.id == effect2?.id)
-                                XCTAssertTrue(effect1?.account == effect2?.account)
-                                XCTAssertTrue(effect1?.effectType == effect2?.effectType)
-                                XCTAssertTrue(effect1?.effectTypeString == effect2?.effectTypeString)
+                            case .success(_):
                                 XCTAssert(true)
                                 expectation.fulfill()
                             case .failure(let error):
-                                StellarSDKLog.printHorizonRequestErrorMessage(tag:"GE Test", horizonRequestError: error)
+                                StellarSDKLog.printHorizonRequestErrorMessage(tag:"getEffectsForOperation", horizonRequestError: error)
                                 XCTAssert(false)
                             }
+                            expectation.fulfill()
                         }
-                    case .failure(let error):
-                        StellarSDKLog.printHorizonRequestErrorMessage(tag:"GE Test", horizonRequestError: error)
-                        XCTAssert(false)
                     }
+                case .failure(let error):
+                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"getEffectsForOperation", horizonRequestError: error)
+                    XCTAssert(false)
                 }
-            case .failure(let error):
-                StellarSDKLog.printHorizonRequestErrorMessage(tag:"GE Test", horizonRequestError: error)
-                XCTAssert(false)
-                expectation.fulfill()
             }
+            wait(for: [expectation], timeout: 15.0)
         }
-        
-        wait(for: [expectation], timeout: 15.0)
     }
     
-    func testGetEffectsForAccount() {
-        let expectation = XCTestExpectation(description: "Get effects for account and parse their details successfuly")
-        
-        sdk.effects.getEffects(forAccount: "GAQXAWHCM4A7SQCT3BOSVEGRI2OOB7LO2CMFOYFF6YRXU4VQSB5V2V2K", order:Order.descending) { (response) -> (Void) in
-            switch response {
-            case .success(_):
-                XCTAssert(true)
-            case .failure(let error):
-                StellarSDKLog.printHorizonRequestErrorMessage(tag:"GEFA Test", horizonRequestError: error)
-                XCTAssert(false)
+    func getEffectsForLedger() {
+        XCTContext.runActivity(named: "getEffectsForLedger") { activity in
+            let expectation = XCTestExpectation(description: "Get effects for ledger")
+            
+            sdk.effects.getEffects(forLedger: String(self.ledger!)) { (response) -> (Void) in
+                switch response {
+                case .success(_):
+                    XCTAssert(true)
+                case .failure(let error):
+                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"getEffectsForLedger", horizonRequestError: error)
+                    XCTAssert(false)
+                }
+                
+                expectation.fulfill()
             }
             
-            expectation.fulfill()
+            wait(for: [expectation], timeout: 15.0)
         }
-        
-        wait(for: [expectation], timeout: 15.0)
     }
     
-    func testGetEffectsForOperation() {
-        let expectation = XCTestExpectation(description: "Get effects for operation")
-        
-        sdk.effects.getEffects(forOperation: "1577782006009857") { (response) -> (Void) in
-            switch response {
-            case .success(_):
-                XCTAssert(true)
-            case .failure(let error):
-                StellarSDKLog.printHorizonRequestErrorMessage(tag:"GEFO Test", horizonRequestError: error)
-                XCTAssert(false)
+    func getEffectsForTransaction() {
+        XCTContext.runActivity(named: "getEffectsForTransaction") { activity in
+            let expectation = XCTestExpectation(description: "Get effects for transaction")
+            
+            sdk.effects.getEffects(forTransaction: self.transactionHash!) { (response) -> (Void) in
+                switch response {
+                case .success(_):
+                    XCTAssert(true)
+                case .failure(let error):
+                    StellarSDKLog.printHorizonRequestErrorMessage(tag:"getEffectsForTransaction", horizonRequestError: error)
+                    XCTAssert(false)
+                }
+                
+                expectation.fulfill()
             }
             
-            expectation.fulfill()
+            wait(for: [expectation], timeout: 15.0)
         }
-        
-        wait(for: [expectation], timeout: 15.0)
     }
-    
-    func testGetEffectsForLedger() {
-        let expectation = XCTestExpectation(description: "Get effects for ledger")
-        
-        sdk.effects.getEffects(forLedger: "367497") { (response) -> (Void) in
-            switch response {
-            case .success(_):
-                XCTAssert(true)
-            case .failure(let error):
-                StellarSDKLog.printHorizonRequestErrorMessage(tag:"GEFL Test", horizonRequestError: error)
-                XCTAssert(false)
-            }
-            
-            expectation.fulfill()
-        }
-        
-        wait(for: [expectation], timeout: 15.0)
-    }
-    
-    func testGetEffectsForTransaction() {
-        let expectation = XCTestExpectation(description: "Get effects for transaction")
-        
-        sdk.effects.getEffects(forTransaction: "b9d0b2292c4e09e8eb22d036171491e87b8d2086bf8b265874c8d182cb9c9020") { (response) -> (Void) in
-            switch response {
-            case .success(_):
-                XCTAssert(true)
-            case .failure(let error):
-                StellarSDKLog.printHorizonRequestErrorMessage(tag:"GEFT Test", horizonRequestError: error)
-                XCTAssert(false)
-            }
-            
-            expectation.fulfill()
-        }
-        
-        wait(for: [expectation], timeout: 15.0)
-    }
-    
-    /*func testEffectsStream() {
-        let expectation = XCTestExpectation(description: "Get response from stream")
-    
-        sdk.effects.stream(for: .allEffects(cursor: nil)).onReceive { (response) -> (Void) in
-            switch response {
-            case .open:
-                break
-            case .response( _, _):
-                expectation.fulfill()
-            case .error( _):
-                break
-            }
-        }
-        
-        wait(for: [expectation], timeout: 15.0)
-    }
-    
-    func testEffectsForAccountStream() {
-        let expectation = XCTestExpectation(description: "Get response from stream")
-        
-        sdk.effects.stream(for: .effectsForAccount(account: "GDQZ4N3CMM3FL2HLYKZPF3JPZX3IRHI3SQKNSTEG6GMEA3OAW337EBA6", cursor: nil)).onReceive { (response) -> (Void) in
-            switch response {
-            case .open:
-                break
-            case .response( _, _):
-                expectation.fulfill()
-            case .error( _):
-                break
-            }
-        }
-        
-        wait(for: [expectation], timeout: 15.0)
-    }
-    
-    func testEffectsForLedgerStream() {
-        let expectation = XCTestExpectation(description: "Get response from stream")
-        
-        sdk.effects.stream(for: .effectsForLedger(ledger: "2365", cursor: nil)).onReceive { (response) -> (Void) in
-            switch response {
-            case .open:
-                break
-            case .response( _, _):
-                expectation.fulfill()
-            case .error( _):
-                break
-            }
-        }
-        
-        wait(for: [expectation], timeout: 15.0)
-    }
-    
-    func testEffectsForOperationStream() {
-        let expectation = XCTestExpectation(description: "Get response from stream")
-        
-        sdk.effects.stream(for: .effectsForOperation(operation: "10157597659137", cursor: nil)).onReceive { (response) -> (Void) in
-            switch response {
-            case .open:
-                break
-            case .response( _, _):
-                expectation.fulfill()
-            case .error( _):
-                break
-            }
-        }
-        
-        wait(for: [expectation], timeout: 15.0)
-    }
-    
-    func testEffectsForTransactionsStream() {
-        let expectation = XCTestExpectation(description: "Get response from stream")
-        
-        sdk.effects.stream(for: .effectsForTransaction(transaction: "17a670bc424ff5ce3b386dbfaae9990b66a2a37b4fbe51547e8794962a3f9e6a", cursor: nil)).onReceive { (response) -> (Void) in
-            switch response {
-            case .open:
-                break
-            case .response( _, _):
-                expectation.fulfill()
-            case .error( _):
-                break
-            }
-        }
-        
-        wait(for: [expectation], timeout: 15.0)
-    }*/
-    
 }
