21b7cc8355ad47f55722131cdb3869095ec0538b
Christian Rogobete <c.rogobete@soneso.com>
2024-09-20 15:56:58 +0200

    add package platforms

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "stellarsdk",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "stellarsdk", 
