84f615f13310ec9bf8cc3c51d95b7224876aef4a
Christian Rogobete <c.rogobete@soneso.com>
2025-05-27 10:37:23 +0200

    feat(sep-2): improve federation test

--- stellarsdk/stellarsdkTests/federation/FederationTestCase.swift
+++ stellarsdk/stellarsdkTests/federation/FederationTestCase.swift
@@ -10,7 +10,18 @@ import XCTest
 import stellarsdk
 
 class FederationTestCase: XCTestCase {
-        
+    
+    let federationServer = "127.0.0.1"
+    var federationMock: FederationResponseMock!
+    
+    override func setUp() {
+        super.setUp()
+                
+        URLProtocol.registerClass(ServerMock.self)
+        federationMock = FederationResponseMock(host: federationServer)
+
+    }
+    
     func testResolveStellarAddress() async {
         let responseEnum = await Federation.resolve(stellarAddress: "bob*soneso.com")
         switch responseEnum {
@@ -26,7 +37,7 @@ class FederationTestCase: XCTestCase {
     
     func testResolveStellarAccountId2() async {
         
-        let federation = Federation(federationAddress: "https://stellarid.io/federation/")
+        let federation = Federation(federationAddress: "https://stellarid.io/federation")
         let responseEnum = await federation.resolve(address: "bob*soneso.com")
         switch responseEnum {
         case .success(let response):
@@ -40,7 +51,7 @@ class FederationTestCase: XCTestCase {
     }
     
     func testResolveStellarAccountId() async {
-        let federation = Federation(federationAddress: "https://stellarid.io/federation/")
+        let federation = Federation(federationAddress: "https://stellarid.io/federation")
         let responseEnum = await federation.resolve(account_id: "GBVPKXWMAB3FIUJB6T7LF66DABKKA2ZHRHDOQZ25GBAEFZVHTBPJNOJI")
         switch responseEnum {
         case .success(let response):
@@ -54,10 +65,10 @@ class FederationTestCase: XCTestCase {
     }
     
     // unfortunately this (transaction_id) is not supported by stellarid.io.
-    // but one can test by debugging and checking the federation request url.
+    // but we can test it with our mock
     func testResolveTransactionId() async {
         
-        let federation = Federation(federationAddress: "https://stellarid.io/federation/")
+        let federation = Federation(federationAddress: "https://127.0.0.1/federation")
         let responseEnum = await federation.resolve(transaction_id: "c1b368c00e9852351361e07cc58c54277e7a6366580044ab152b8db9cd8ec52a")
         switch responseEnum {
         case .success(let response):
@@ -71,10 +82,10 @@ class FederationTestCase: XCTestCase {
     }
     
     // unfortunately this (forward) is not supported by stellarid.io.
-    // but one can test by debugging and checking the federation request url.
+    // but we can test it with our mock
     func testResolveForward() async {
         
-        let federation = Federation(federationAddress: "https://stellarid.io/federation/")
+        let federation = Federation(federationAddress: "https://127.0.0.1/federation")
         
         var params = Dictionary<String,String>()
         params["forward_type"] = "bank_account"
@@ -92,4 +103,51 @@ class FederationTestCase: XCTestCase {
             XCTFail()
         }
     }
+}
+
+class FederationResponseMock: ResponsesMock {
+    var host: String
+    
+    init(host:String) {
+        self.host = host
+        super.init()
+    }
+    
+    override func requestMock() -> RequestMock {
+        let handler: MockHandler = { [weak self] mock, request in
+            if let type = mock.variables["type"]  {
+                if type == "txid" {
+                    if let q = mock.variables["q"], q == "c1b368c00e9852351361e07cc58c54277e7a6366580044ab152b8db9cd8ec52a" {
+                        mock.statusCode = 200
+                        return self?.success
+                    }
+                } else if type == "forward" {
+                    if let forwardType = mock.variables["forward_type"], forwardType == "bank_account",
+                        let swift = mock.variables["swift"], swift == "BOPBPHMM",
+                       let acct = mock.variables["acct"], acct == "2382376" {
+                        mock.statusCode = 200
+                        return self?.success
+                    }
+                }
+            }
+                
+            mock.statusCode = 400
+            return nil
+        }
+        
+        return RequestMock(host: host,
+                           path: "/federation",
+                           httpMethod: "GET",
+                           mockHandler: handler)
+    }
+    
+    let success = """
+    {
+      "stellar_address": "bob*soneso.com",
+      "account_id": "GBVPKXWMAB3FIUJB6T7LF66DABKKA2ZHRHDOQZ25GBAEFZVHTBPJNOJI",
+      "memo_type": "text",
+      "memo": "hello memo text"
+    }
+    """
+    
 }
